cc7dce6aa0787c680f4ba5297198deccb52f288a
pv-kiran <kiranpv903@gmail.com>
2022-12-15 17:26:07 +0530

    client side validation

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,1 +1,50 @@
 console.log("Hello");
+
+var form = document.querySelector("#form");
+
+form.addEventListener('submit' , function(e) {
+    e.preventDefault(); 
+    let isValid = validate()
+    if(isValid) {
+        document.querySelector("#form").submit();
+    }
+})
+
+function validate() {
+    var name = document.querySelector('#name').value;
+    var email = document.querySelector('#email').value;
+    var password = document.querySelector('#password').value;
+    var err = document.querySelector('.error');
+    var text ;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+
+    
+
+    if( name.length < 5 ) {
+       text = "Please enter a valid name";
+       err.textContent = text ;
+       err.style.height = '4rem';
+       return false;
+    }
+    else if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+      text = 'Write full name';
+      err.textContent = text;
+      err.style.height = '4rem';
+      return false;
+    }
+    else if(email.match(mailformat) === null) {
+        text = "Please enter a valid email";
+        err.textContent = text ;
+        err.style.height = '4rem';
+        return false;
+    }
+    else if(password.length < 5) {
+        text = "Please enter a valid email";
+        err.textContent = text ;
+        err.style.height = '4rem';
+        return false;
+    }
+    
+    return true;
+}
